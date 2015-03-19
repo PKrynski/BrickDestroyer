915d3b97e52498c3c81671e63e2623d0c8149892
Pawel Krynski <krynskip@ee.pw.edu.pl>
2015-03-19 17:22:59 +0100

    Paddle movement & hide mouse pointer

--- bounce_ball.py
+++ bounce_ball.py
@@ -14,7 +14,7 @@ screen = pygame.display.set_mode(size)
 # Set the game window name
 pygame.display.set_caption("Bouncing Ball")
  
-# Loop until the user clicks the close button
+# Run the game until the user clicks the close button
 exit = False
 
 # Continue the game while the user has lives
@@ -23,12 +23,13 @@ game_over = False
 # Used to manage how fast the screen updates
 clock = pygame.time.Clock()
  
-# Starting position of the ball
-ball_x = 300
+# Ball starting position and size (radius)
+ball_rad = 8
+ball_x = 150
 ball_y = 200
 
 # Paddle starting position and dimensions
-pad_width = 70
+pad_width = 68
 pad_height = 10
 pad_x = size[0]/2 - pad_width/2
 pad_y = 460
@@ -47,9 +48,12 @@ while exit == False:
  
 	# Set the screen background color
 	screen.fill(WHITE)
+	
+	# Hide mouse pointer
+	pygame.mouse.set_visible(0)
  
-	# Draw the ball around the center point, radius = 8 pixels
-	pygame.draw.circle(screen, BLUE, (ball_x, ball_y), 8)
+	# Draw the ball around the center point
+	pygame.draw.circle(screen, BLUE, (ball_x, ball_y), ball_rad)
 	
 	# Draw the paddle
 	paddle = pygame.Rect(pad_x, pad_y, pad_width, pad_height)
@@ -58,15 +62,35 @@ while exit == False:
 	# Move the ball
 	ball_x += ball_change_x
 	ball_y += ball_change_y
+	
+	ball_left = ball_x - ball_rad
+	ball_right = ball_x + ball_rad
+	ball_top = ball_y - ball_rad
+	ball_bottom = ball_y + ball_rad
  
-	# Bounce the ball if needed
-	if ball_y >= 470 or ball_y <= 10:
-		ball_change_y *= -1
+	# Move the paddle
+	key = pygame.key.get_pressed()
+	
+	if key[pygame.K_LEFT] == True and pad_left > 0:
+		pad_x -= 2
+	elif key[pygame.K_RIGHT] == True and pad_right < size[0]:
+		pad_x += 2
+	
+	pad_left = pad_x
+	pad_right = pad_x + pad_width
+	
+	# Bounce the ball off the sides
 	if ball_x >= 710 or ball_x <= 10:
 		ball_change_x *= -1
-
-	# Show current position of the ball
-	print "X: %d Y: %d" % (ball_x, ball_y)
+	if  ball_y <= 10:
+		ball_change_y *= -1
+	
+	# Bounce the ball off the paddle
+	if ball_bottom == pad_y and ball_x > pad_left and ball_x < pad_right:
+		ball_change_y *= -1
+	
+	# Show current position of the ball and paddle
+	print "X: %d Y: %d    PAD LEFT: %d  PAD RIGHT: %d" % (ball_x, ball_y, pad_left, pad_right)
 	
 	# Limit updates to 120 frames per second
 	clock.tick(120)
