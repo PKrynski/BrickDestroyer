e57c94d06817ef281033132d902482fc04f0c428
Pawel Krynski <krynskip@ee.pw.edu.pl>
2015-03-25 20:32:17 +0100

    Paddle bounce variation - visual representation

--- DestroyerGame.py
+++ DestroyerGame.py
@@ -5,7 +5,8 @@ WHITE  = ( 255, 255, 255)
 BLACK  = (   0,   0,   0)
 BLUE   = (   0,   0, 255)
 RED    = ( 255,   0,   0)
- 
+GRAY   = ( 200, 200, 200)
+
 pygame.init()
   
 # Set the height and width of the screen
@@ -28,7 +29,7 @@ lives = 3
 clock = pygame.time.Clock()
  
 # Paddle starting position and dimensions
-pad_width = 270
+pad_width = 360
 pad_height = 10
 pad_x = size[0]/2 - pad_width/2
 pad_y = 460
@@ -125,25 +126,25 @@ while exit == False:
 			
 			# Bounce to the right
 			if ball_x >= pad_x + pad_width/2 and ball_x <= pad_x + pad_width:
-				if ball_x > pad_x + 5/6 * pad_width:
+				if ball_x > pad_x + 5/6 * pad_width:		# +60
 					ball_change_x = 1.22
 					ball_change_y = -0.70
-				elif ball_x > pad_x + 4/6 * pad_width:
+				elif ball_x > pad_x + 4/6 * pad_width:		# +45
 					ball_change_x = 1
 					ball_change_y = -1
-				else:
+				else:										# +30
 					ball_change_x = 0.70
 					ball_change_y = -1.22
 			
 			# Bounce to the left
 			if ball_x >= pad_x and ball_x <= pad_x + pad_width/2:
-				if ball_x < pad_x + 1/6 * pad_width:
+				if ball_x < pad_x + 1/6 * pad_width:		# -60
 					ball_change_x = -1.22
 					ball_change_y = -0.70
-				elif ball_x < pad_x + 2/6 * pad_width:
+				elif ball_x < pad_x + 2/6 * pad_width:		# -45
 					ball_change_x = -1
 					ball_change_y = -1
-				else:
+				else:										# -30
 					ball_change_x = -0.70
 					ball_change_y = -1.22
 
@@ -154,6 +155,10 @@ while exit == False:
 	# Draw the paddle
 	paddle = pygame.Rect(pad_x, pad_y, pad_width, pad_height)
 	pygame.draw.rect(screen, RED, paddle)
+	paddle2 = pygame.Rect(pad_x+60, pad_y, 0.66*pad_width, pad_height)
+	pygame.draw.rect(screen, BLUE, paddle2)
+	paddle3 = pygame.Rect(pad_x+120, pad_y, 0.33*pad_width, pad_height)
+	pygame.draw.rect(screen, GRAY, paddle3)
 	
 	# Show angle
 	if ball_change_x == 0:
